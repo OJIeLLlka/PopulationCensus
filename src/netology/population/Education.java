f3b1fdfdf164899f404b6561838c76ea160d7797
OJIeLLlka <olbastnie@gmail.com>
2022-03-08 18:28:19 +0300

    Initial commit

---
+++ src/netology/population/Education.java
@@ -0,0 +1,8 @@
+package netology.population;
+
+public enum Education {
+    ELEMENTARY,
+    SECONDARY,
+    FURTHER,
+    HIGHER
+}
---
+++ src/netology/population/Sex.java
@@ -0,0 +1,6 @@
+package netology.population;
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
